828ac9f4d802f92199520982c402a0c305ce1af8
Michael Ekins <mekins86@gmail.com>
2016-03-09 22:02:02 +0000

    Initial commit

---
+++ DuplicateLineRemover/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace DuplicateLineRemover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sourceFile = args[0];
+
+            var destinationPath = Path.GetFileNameWithoutExtension(sourceFile) + "_output" + Path.GetExtension(sourceFile);
+
+            var previousLines = new HashSet<string>();
+
+            File.WriteAllLines(destinationPath, File.ReadLines(sourceFile)
+                                                    .Where(line => previousLines.Add(line)));
+        }
+    }
+}
